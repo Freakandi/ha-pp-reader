cb210628a6ef413df6f03a10509bbc5e37ecec8c
Freakandi <a.luehnen@kabelmail.de>
2025-05-03 19:04:07 +0000

    Add data path

--- custom_components/pp_reader/__init__.py
+++ custom_components/pp_reader/__init__.py
@@ -13,7 +13,7 @@ from homeassistant.components.http import StaticPathConfig, HomeAssistantView
 from .backup_db import setup_backup_system
 from .const import DOMAIN, CONF_API_TOKEN, CONF_FILE_PATH, CONF_DB_PATH
 from .data.reader import parse_data_portfolio
-from .db_init import initialize_database_schema
+from .data.db_init import initialize_database_schema
 
 import asyncio
 from functools import partial
