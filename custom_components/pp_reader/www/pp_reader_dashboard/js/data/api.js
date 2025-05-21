4b886413e2b6990c0ba3b916c5e94713a1055237
Freakandi <not@disclosed.com>
2025-05-21 13:15:26 +0200

    chore: update code before version bump

--- custom_components/pp_reader/www/pp_reader_dashboard/js/data/api.js
+++ custom_components/pp_reader/www/pp_reader_dashboard/js/data/api.js
@@ -5,22 +5,22 @@ export async function fetchStates() {
 }
 
 export async function fetchDashboardDataWS() {
-  let conn = null;
+  let hass = null;
 
-  // Verbindung zur Home Assistant Websocket-API herstellen
-  if (window.parent && window.parent.hassConnection) {
-    conn = await window.parent.hassConnection;
-  } else if (window.hassConnection) {
-    conn = await window.hassConnection;
+  // Zugriff auf das Home Assistant-Objekt
+  if (window.parent && window.parent.hass) {
+    hass = window.parent.hass;
+  } else if (window.hass) {
+    hass = window.hass;
   }
 
-  if (!conn) {
+  if (!hass || !hass.connection) {
     throw new Error("Keine gültige Home Assistant Websocket-Verbindung gefunden! Bitte das Dashboard als Panel in Home Assistant öffnen.");
   }
 
   // Websocket-Nachricht senden
   try {
-    const response = await conn.sendMessage({
+    const response = await hass.connection.sendMessagePromise({
       type: "pp_reader/get_dashboard_data"
     });
     return response;
